64d8538a218a1678520d543333c765db35397c35
Lucas Gray <soulcatalyst@gmail.com>
2018-10-20 13:21:16 -0700

    docs updates and beginning test data.

---
+++ inst/testdata/build_testdata.R
@@ -0,0 +1,7 @@
+library(lowcat)
+
+inst_bams <- c(system.file("inst/testdata/bam/", "N702-N504_S13_L001_R1_001.rmd.srt.bam", package = "lowcat"),
+               system.file("inst/testdata/bam/", "N702-N506_S15_L001_R1_001.rmd.srt.bam", package = "lowcat"),
+               system.file("inst/testdata/bam/", "N702-N507_S16_L001_R1_001.rmd.srt.bam", package = "lowcat"))
+
+bam_to_fragment_list(inst_bams)
